813f465713aa9b4bc824b1b8b713da9b417ec247
Ubuntu <ubuntu@ip-172-31-24-229.us-west-1.compute.internal>
2023-10-08 15:36:13 +0000

    add deploy shell script (have not tested yet)

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+# pull the newest static webpages
+cd static-webpages
+git pull
+
+wait
+
+# copy the generated site folder to the templates folder of django project
+cd ..
+cp -r static-webpages/site/ source/content/templates/
+cd source/content/templates/
+rm -rf main
+mv site main
+
+# when first time launch the gunicorn process, use these 4 lines
+#cd ~/dagrad-site/source
+#conda activate dagrad
+#gunicorn --reload -w 4 --bind=0.0.0.0:8000 app.wsgi -D
+
+# restart the running gunicorn process
+pid=$(pstree -ap | grep -m 1 gunicorn | awk -F ',' '{print $2}' | awk -F ' ' '{print $1}’)
+kill -HUP pid
+
